4104b2b33d4fa6fa94a1f91648d1877a04c60941
assemtynyshtybay <asem201000@gmail.com>
2019-02-07 21:19:36 +0600

    Task3

---
+++ Week2/Task3/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+namespace Task3
+{
+    class Program
+    {
+         static void Main(string[] args)
+        {
+            Way();
+            Console.ReadKey();
+        }
+
+        private static void PrintInfo(FileSystemInfo sp, int k)
+        {
+            string path = new string(' ', k);
+            path = path + sp.Name;
+            Console.WriteLine(path);
+
+            if(sp.GetType() == typeof(DirectoryInfo))
+            {
+                var items = (sp as DirectoryInfo).GetFileSystemInfos();
+                foreach (var i in items)
+                {
+                    PrintInfo(i, k + 6);
+                }
+            }
+        }
+
+        private static void Way()
+        {
+            DirectoryInfo dir = new DirectoryInfo(@"C:\Users\Aser\Desktop\cake");
+            PrintInfo(dir, 1);
+        }
+    }
+}
