7a4d773f1336feec0c16661783ba427c0c41a301
assemtynyshtybay <asem201000@gmail.com>
2019-02-08 20:09:18 +0600

    changes Task2

--- Week1/Task2/Program.cs
+++ Week1/Task2/Program.cs
@@ -6,99 +6,46 @@ using System.Threading.Tasks;
 
 namespace Task2
 {
-    //class Student
-   //{
-           class Student3
-    {
-        public string GPA
-        {
-            get;
-            set;
-        }
-    }
+     class Program
+{
 
-    class Student2
-    {
-        private string gpa;
-        public string GPA
+      class Student
+      {
+        private string name;
+        private string id;
+        private int yearos;
+        public Student(string name,string id)
         {
-            get { return gpa; }
+           this.name = name;
+            this.id=id;
+        }
+        public void increase()
+        {
+            yearos++;
+        }
+        public int Yearos
+            {
+            get
+            {
+                return yearos;
+            }
             set
             {
-                if (value.Length < 2)
-                {
-
-                }
-                else
-                {
-                    gpa = value;
-                }
+                yearos=value;
             }
-        }
-    }
-    class Student
-    {
-        private string gpa;
-        public void SetGPA(string g)
-        {
-            if (g.Length < 2)
-            {
-
             }
-            else
-            {
-                gpa = g;
-            }
-        }
-        public string GetGPA()
-        {
-            return gpa;
-        }
-    }
-
-    class Program
-    {
+        
+   
         static void Main(string[] args)
-        {
-            Student s1 = new Student();
-            s1.SetGPA("3.6");
-            s1.SetGPA("3");
-            Console.WriteLine(s1.GetGPA());
-
-            Student2 s2 = new Student2();
-            s2.GPA = "3.6";
-            s2.GPA = "3";
-            Console.WriteLine(s2.GPA);
-
-            Student2 s22 = new Student2 { GPA = "4.0" };
-            Console.WriteLine(s22.GPA);
-
-            Student3 s3 = new Student3 { GPA = "2.0" };
-            Console.WriteLine(s3.GPA);
-                //public string name = Console.ReadLine();
-           //  public string id =Console.ReadLine();
-            // public string year=Console.ReadLine();
-           //  public Student(string n,string i,string y)
-             
-              
-              
-   // }
-
-       // public void PrintInfo()
-           // {
-          /*  Console.WriteLine(name+" "+id+" "+year);
-            }
-      class Program
-      {
-        static void Main(string[] args)
-        {   
-            Student s=new Student(name+" "+id+" "+year);
-            //s.name="Assem";
-            //s.id="18BD0727";
-            //s.year="1";
-            s.PrintInfo();*/
+        {    
+            Student s=new Student("Assem","18BD23456");
+            s.yearos=2018;
+            s.increase();
+            
+            Console.WriteLine(s.yearos);
                 Console.ReadKey();
         }
-      }
-    
+      
+    }
+ }
 }
