5d503a0a13f1dca1f4713a757c02c6465872c9d0
assemtynyshtybay <asem201000@gmail.com>
2019-02-15 20:57:11 +0600

    add2

--- lab3/Program.cs
+++ lab3/Program.cs
@@ -15,11 +15,6 @@ namespace lab3
             DirectoryInfo directory = null;
             FileSystemInfo currentFs = null;
 
-            public FarManager()
-            {
-                cursor = 0;
-            }
-
             public FarManager(string path)
             {
                 this.path = path;
@@ -29,7 +24,7 @@ namespace lab3
                 ok = true;
             }
 
-            public void Color(FileSystemInfo fs, int index)
+            public void Color(FileSystemInfo fs, int index)//choice colors
             {
                 if (cursor == index)
                 {
@@ -49,38 +44,37 @@ namespace lab3
                 }
             }
 
-            public void Show()
+            public void Show() //write in output name of directories or files in order with number
             {
                 Console.BackgroundColor = ConsoleColor.Black;
                 Console.Clear();
                 directory = new DirectoryInfo(path);
                 FileSystemInfo[] fs = directory.GetFileSystemInfos();
-                for (int i = 0, k = 0; i < fs.Length; i++)
+                for (int i = 0; i < fs.Length; i++)
                 {
                     if (ok == false && fs[i].Name[0] == '.')//avoid from hidden files
                     {
                         continue;
                     }
-                    Color(fs[i], k);
+                    Color(fs[i], i);
                     Console.Write(i + 1 + ".");
                     Console.WriteLine(fs[i].Name);
-                    k++;
                 }
             }
-            public void Up()
+            public void Up()//motion "up"
             {
                 cursor--;
                 if (cursor < 0)
                     cursor = sz - 1;
             }
-            public void Down()
+            public void Down()//motion "down"
             {
                 cursor++;
                 if (cursor == sz)
                     cursor = 0;
             }
 
-            public void CalcSz()
+            public void CalcSz()//don't write index of hidden file
             {
                 directory = new DirectoryInfo(path);
                 FileSystemInfo[] fs = directory.GetFileSystemInfos();
@@ -90,52 +84,78 @@ namespace lab3
                         if (fs[i].Name[0] == '.')
                             sz--;
             }
-
-            public void Start() //fundamental method
+            public void Opentxt(string name) //open text file
             {
-                ConsoleKeyInfo consoleKey = Console.ReadKey();
-                while (consoleKey.Key != ConsoleKey.Escape)
-                {
-                    CalcSz();
-                    Show();
-                    consoleKey = Console.ReadKey();
-                    if (consoleKey.Key == ConsoleKey.UpArrow)
-                        Up();
-                    if (consoleKey.Key == ConsoleKey.DownArrow)
-                        Down();
-                    if (consoleKey.Key == ConsoleKey.Enter)
-                    {
-                        if (currentFs.GetType() == typeof(DirectoryInfo))
-                        {
-                            cursor = 0;
-                            path = currentFs.FullName;
-                        }
-                    }
-                    if (consoleKey.Key == ConsoleKey.Backspace)
-                    {
-                        cursor = 0;
-                        path = directory.Parent.FullName;
-                    }
-                    if (consoleKey.Key==ConsoleKey.Delete)
-                    {
-                        cursor = 0;
-                        if (currentFs.GetType() == typeof(DirectoryInfo))
-                        Directory.Delete(currentFs.FullName, true);
-                        currentFs.Delete();
-                        
-                    }
-                }
+                    Console.BackgroundColor = ConsoleColor.White;
+                    Console.ForegroundColor = ConsoleColor.Black;
+                    Console.Clear();
+                    StreamReader sr = new StreamReader(name);
+                    string red = sr.ReadToEnd();
+                    Console.WriteLine(red);
+                    ConsoleKeyInfo n = Console.ReadKey();
+                     if (n.Key == ConsoleKey.Backspace)
+                     {
+                        sr.Close();
+                        return;
+                     }
+                     else
+                        Opentxt(name);
             }
             
+            public void Start() //fundamental method
+            {
+              ConsoleKeyInfo consoleKey = Console.ReadKey();
+              while (consoleKey.Key != ConsoleKey.Escape)//when press "Esc" program close 
+              {
+                CalcSz();//method that delete place of hidden file and decrise length
+                Show();//method to show in output directories or files
+                consoleKey = Console.ReadKey(); //defining a button
+                if (consoleKey.Key == ConsoleKey.UpArrow)
+                    Up();
+                if (consoleKey.Key == ConsoleKey.DownArrow)
+                    Down();
+                if (consoleKey.Key == ConsoleKey.Enter)//opening directories ,files
+                {
+                    if (currentFs.GetType() == typeof(DirectoryInfo))
+                    {
+                        cursor = 0;
+                        path = currentFs.FullName;
+                    }
+                    else if (currentFs.Name.EndsWith(".txt"))//if find text file open it
+                        Opentxt(currentFs.FullName);//method of opening text file
+                }
+                if (consoleKey.Key == ConsoleKey.Backspace)//motion back
+                {
+                    cursor = 0;
+                    path = directory.Parent.FullName;
+                }
+                if (consoleKey.Key == ConsoleKey.Delete) //deleting directories or files
+                {
+                    cursor = 0;
+                    if (currentFs.GetType() == typeof(DirectoryInfo))
+                        Directory.Delete(currentFs.FullName, true);
+                    currentFs.Delete();
 
+                }
+                if(consoleKey.Key==ConsoleKey.Home)//renaming directories or files
+                {
+                    string fn = Console.ReadLine();
+                    fn = Path.Combine(directory.FullName, fn);
+                    if (currentFs.GetType() == typeof(DirectoryInfo))
+                        Directory.Move(currentFs.FullName, fn);
+                    else
+                        File.Move(currentFs.FullName, fn);
+                }
+              }
+            }
         }
 
         class Program
         {
             static void Main(string[] args)
             {
-                string path = @"C:\Users\Aser\Desktop\cake";
-                FarManager farManager = new FarManager(path);
+                string path = @"C:\Users\Aser\Desktop\cake";//point into path
+                FarManager farManager = new FarManager(path);//create farmanager
                 farManager.Start();//go to method Start
             }
         }
